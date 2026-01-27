cf393f39982ec58216760813df33157fc698606a
Ciphermoth <courtney.malone@colostate.edu>
2026-01-27 15:27:58 -0700

    Update ReleaseHelpers.groovy

--- src/release/ReleaseHelpers.groovy
+++ src/release/ReleaseHelpers.groovy
@@ -1,4 +1,3 @@
-
 package release
 
 class ReleaseHelpers implements Serializable {
@@ -8,11 +7,17 @@ class ReleaseHelpers implements Serializable {
         this.steps = steps
     }
 
+    // ---------------------------------------------------------
+    // CHECKOUT LOGIC
+    // ---------------------------------------------------------
     def checkoutAllComponents() {
         steps.echo "Checking out all repositories..."
         // TODO: implement checkout logic
     }
 
+    // ---------------------------------------------------------
+    // VERSION LOADING + CALCULATION
+    // ---------------------------------------------------------
     def loadReleaseVersion() {
         steps.echo "Loading release-version.json..."
         // TODO: implement Nexus download + JSON parse
@@ -23,6 +28,9 @@ class ReleaseHelpers implements Serializable {
         // TODO: implement version bump logic
     }
 
+    // ---------------------------------------------------------
+    // BRANCH CREATION
+    // ---------------------------------------------------------
     def createReleaseCandidateBranch() {
         steps.echo "Creating release-candidate branch..."
         // TODO: implement git branch + push
@@ -33,38 +41,60 @@ class ReleaseHelpers implements Serializable {
         // TODO: implement official release branch creation
     }
 
+    // ---------------------------------------------------------
+    // METADATA GENERATION
+    // ---------------------------------------------------------
     def generateMetadataFiles() {
         steps.echo "Generating metadata files..."
         // TODO: write JSON, TXT, CSV metadata
     }
 
+    // ---------------------------------------------------------
+    // SUBMODULE PULL
+    // ---------------------------------------------------------
     def pullDeploySubmodules(isCandidate) {
         steps.echo "Pulling NTSPDDeploy submodules..."
         // TODO: git submodule update logic
     }
 
+    // ---------------------------------------------------------
+    // RELEASE NOTES
+    // ---------------------------------------------------------
     def generateReleaseNotes() {
         steps.echo "Generating release notes..."
         // TODO: git log > release-notes.txt
     }
 
+    // ---------------------------------------------------------
+    // ARTIFACT GENERATION
+    // ---------------------------------------------------------
     def generateArtifacts() {
         steps.echo "Generating tar and ISO artifacts..."
         // TODO: tar + ISO build commands
     }
 
+    // ---------------------------------------------------------
+    // NEXUS UPLOAD
+    // ---------------------------------------------------------
     def uploadArtifactsToNexus() {
         steps.echo "Uploading artifacts to Nexus..."
         // TODO: curl or Nexus CLI upload
     }
 
+    // ---------------------------------------------------------
+    // DEPLOYMENT
+    // ---------------------------------------------------------
     def runDeployment() {
         steps.echo "Running deployment..."
         // TODO: deployment script or ansible call
     }
 
+    // ---------------------------------------------------------
+    // CLEANUP
+    // ---------------------------------------------------------
     def cleanUpReleaseWorkspace() {
         steps.echo "Cleaning workspace..."
         steps.cleanWs()
     }
 }
+
