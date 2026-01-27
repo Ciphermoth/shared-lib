e6d83d592033e035024792600b88df5d8693f4ec
Ciphermoth <courtney.malone@colostate.edu>
2026-01-27 15:20:21 -0700

    Update ReleaseHelpers.groovy

--- src/release/ReleaseHelpers.groovy
+++ src/release/ReleaseHelpers.groovy
@@ -1,1 +1,70 @@
 
+package release
+
+class ReleaseHelpers implements Serializable {
+
+    def steps
+    ReleaseHelpers(steps) {
+        this.steps = steps
+    }
+
+    def checkoutAllComponents() {
+        steps.echo "Checking out all repositories..."
+        // TODO: implement checkout logic
+    }
+
+    def loadReleaseVersion() {
+        steps.echo "Loading release-version.json..."
+        // TODO: implement Nexus download + JSON parse
+    }
+
+    def determineVersionChanges() {
+        steps.echo "Determining version changes..."
+        // TODO: implement version bump logic
+    }
+
+    def createReleaseCandidateBranch() {
+        steps.echo "Creating release-candidate branch..."
+        // TODO: implement git branch + push
+    }
+
+    def createOfficialReleaseBranch() {
+        steps.echo "Creating official release branch..."
+        // TODO: implement official release branch creation
+    }
+
+    def generateMetadataFiles() {
+        steps.echo "Generating metadata files..."
+        // TODO: write JSON, TXT, CSV metadata
+    }
+
+    def pullDeploySubmodules(isCandidate) {
+        steps.echo "Pulling NTSPDDeploy submodules..."
+        // TODO: git submodule update logic
+    }
+
+    def generateReleaseNotes() {
+        steps.echo "Generating release notes..."
+        // TODO: git log > release-notes.txt
+    }
+
+    def generateArtifacts() {
+        steps.echo "Generating tar and ISO artifacts..."
+        // TODO: tar + ISO build commands
+    }
+
+    def uploadArtifactsToNexus() {
+        steps.echo "Uploading artifacts to Nexus..."
+        // TODO: curl or Nexus CLI upload
+    }
+
+    def runDeployment() {
+        steps.echo "Running deployment..."
+        // TODO: deployment script or ansible call
+    }
+
+    def cleanUpReleaseWorkspace() {
+        steps.echo "Cleaning workspace..."
+        steps.cleanWs()
+    }
+}
