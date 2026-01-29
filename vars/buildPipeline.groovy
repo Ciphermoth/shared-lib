a6733c7bd71b9a0917ce8230c9002fd2ee532be9
Ciphermoth <courtney.malone@colostate.edu>
2026-01-28 19:24:03 -0700

    Implement project type detection in buildPipeline
    Added a function to detect project types and build accordingly.

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,1 +1,21 @@
+def call() {
 
+    echo "Building application..."
+
+    boolean isCpp = fileExists('CMakeLists.txt')
+    boolean isGradle = fileExists('build.gradle') || fileExists('build.gradle.kts')
+
+    if (isCpp) {
+        echo "Detected C++ project"
+        echo "C++ build would run here"
+        return
+    }
+
+    if (isGradle) {
+        echo "Detected Gradle project"
+        echo "Gradle build would run here"
+        return
+    }
+
+    error "Unknown project type"
+}
